80b80c7bdc41e4c8fba6bcdad8d671d66ab6b740
atamanbillor <119147106+atamanbillor@users.noreply.github.com>
2022-11-27 00:14:10 -0800

    Add files via upload

---
+++ ex7.py
@@ -0,0 +1,38 @@
+
+# the print command prints the string in quotations
+
+print("Mary had a little lamb.")
+
+#the .format function appears here and will insert the string snow
+# into the squigglies{}
+
+print("Its fleece was white as {}.".format('snow'))
+print("And everywhere that Mary went.")
+
+# the '.' will be printed but the *10 will enable there to be 10 .'s
+
+print("." * 10)
+
+# assigning each letter of Cheese Burger to variable endx
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch end = ' '  at the end. Try removing it to see what happens
+# it is acting as a space between Cheese Burger! wow! amazing
+# coding is cool
+
+#adds all of the endx strings together and forms Cheese Burger!
+
+print(end1 + end2 + end3 + end4 + end5 + end6, end =' ')
+print(end7 + end8 + end9 + end10 + end11 + end12)
