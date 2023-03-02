7c2fd70d81a54fe9be41986d51ee30b478a00ee9
atamanbillor <119147106+atamanbillor@users.noreply.github.com>
2023-03-01 19:40:04 -0800

    Add files via upload

---
+++ ex20SD2.py
@@ -0,0 +1,68 @@
+# pulling the function "import" from the sys python package
+
+from sys import argv
+
+#defining the argument variables
+
+script, input_file = argv
+
+#defining our first function
+# this function prints the file that is being read
+#the "f" denotes that you are calling the functions from a file
+
+def print_all(f):
+    print(f.read())
+
+# defining the second function
+# the seek function basically just moves the header to the byte that you specify it to
+
+def rewind(f):
+    f.seek(0)
+
+# defining the third function
+# the "readline" function reads one line at a time for the given file
+
+def print_a_line(line_count, f):
+    print(line_count, f.readline())
+
+#opening the input file and defining it as a new variable
+
+current_file = open(input_file)
+
+print("First let's print the whole file:\n")
+
+#calling the first defined function that will print out all of the file
+
+print_all(current_file)
+
+print("Now let's rewind, kind of like a tape.")
+
+#calling the second defined function
+
+rewind(current_file)
+
+print("Let's print three lines:")
+
+# printing one line at a time
+# This format is repetitive and a lot of work
+# best to use a loop in the future ;)
+
+current_line = 1
+print_a_line(current_line, current_file)
+
+print(f"The current line we are printing is {current_line}\n")
+# now we want to print one line at time. so we are going to add 1 to the line count as it progresses
+# and then we will give that rising line count to the function we just defined and it will print the respective lines
+
+current_line += 1
+print_a_line(current_line, current_file)
+
+print(f"The current line we are printing is {current_line}\n")
+
+#continuing to add 1 to the line count with the '+=' shorthand
+# the '+=' shorthand stands for currentline + 1 = currentline
+
+current_line += 1
+print_a_line(current_line, current_file)
+
+print(f"The current line we are printing is {current_line}\n")
